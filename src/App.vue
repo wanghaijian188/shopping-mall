1a12d4da31b20bc3fbfede64536dc3b1e9915af5
wanghaijan <1469202826@qq.com>
2019-10-19 09:45:44 +0800

    导航

--- src/App.vue
+++ src/App.vue
@@ -27,13 +27,13 @@ export default {
   color: #2c3e50;
 }
 #nav {
-  padding: 30px;
   a {
     font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+    text-decoration: none;
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+   
   }
 }
 </style>
--- src/components/HeadTop.vue
+++ src/components/HeadTop.vue
@@ -1,59 +1,30 @@
 <template>
   <div id="nav">
-    <div>
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
-    </div>
-    <div class="page-component__nav el-scrollbar">
-      <div class="el-scrollbar__wrap" style="margin-bottom:-17px;margin-right:-17px">
-        <div class="el-scrollbar__view">
-          <div class="side-nav" style="opacity:1">
-            <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
-              <el-radio-button :label="false">展开</el-radio-button>
-              <el-radio-button :label="true">收起</el-radio-button>
-            </el-radio-group>
-            <el-menu
-              default-active="1-4-1"
-              class="el-menu-vertical-demo"
-              @open="handleOpen"
-              @close="handleClose"
-              :collapse="isCollapse"
-            >
-              <el-submenu index="1">
-                <template slot="title">
-                  <i class="el-icon-location"></i>
-                  <span slot="title">导航一</span>
-                </template>
-                <el-menu-item-group>
-                  <span slot="title">分组一</span>
-                  <el-menu-item index="1-1">选项1</el-menu-item>
-                  <el-menu-item index="1-2">选项2</el-menu-item>
-                </el-menu-item-group>
-                <el-menu-item-group title="分组2">
-                  <el-menu-item index="1-3">选项3</el-menu-item>
-                </el-menu-item-group>
-                <el-submenu index="1-4">
-                  <span slot="title">选项4</span>
-                  <el-menu-item index="1-4-1">选项1</el-menu-item>
-                </el-submenu>
-              </el-submenu>
-              <el-menu-item index="2">
-                <i class="el-icon-menu"></i>
-                <span slot="title">导航二</span>
-              </el-menu-item>
-              <el-menu-item index="3" disabled>
-                <i class="el-icon-document"></i>
-                <span slot="title">导航三</span>
-              </el-menu-item>
-              <el-menu-item index="4">
-                <i class="el-icon-setting"></i>
-                <span slot="title">导航四</span>
-              </el-menu-item>
-            </el-menu>
-          </div>
-        </div>
-      </div>
-    </div>
+    <el-menu
+  :default-active="activeIndex2"
+  class="el-menu-demo"
+  mode="horizontal"
+  @select="handleSelect"
+  background-color="#545c64"
+  text-color="#fff"
+  active-text-color="#ffd04b" style="padding:0 160px">
+  <el-menu-item index="1" > <router-link to="/">Home</router-link></el-menu-item>
+   <el-menu-item index="5" > <router-link to="/about">About</router-link></el-menu-item>
+  <el-submenu index="2">
+    <template slot="title">我的工作台</template>
+    <el-menu-item index="2-1">c</el-menu-item>
+    <el-menu-item index="2-2">选项2</el-menu-item>
+    <el-menu-item index="2-3">选项3</el-menu-item>
+    <el-submenu index="2-4">
+      <template slot="title">选项4</template>
+      <el-menu-item index="2-4-1">选项1</el-menu-item>
+      <el-menu-item index="2-4-2">选项2</el-menu-item>
+      <el-menu-item index="2-4-3">选项3</el-menu-item>
+    </el-submenu>
+  </el-submenu>
+  <el-menu-item index="3" disabled>消息中心</el-menu-item>
+ 
+</el-menu>
   </div>
 </template>
 
@@ -70,18 +41,16 @@
 
 <script>
 export default {
-  data() {
-    return {
-      isCollapse: true
-    };
-  },
-  methods: {
-    handleOpen(key, keyPath) {
-      console.log(key, keyPath);
+ data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      };
     },
-    handleClose(key, keyPath) {
-      console.log(key, keyPath);
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
     }
-  }
 };
 </script>
