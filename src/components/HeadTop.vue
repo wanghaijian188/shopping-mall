e26b86294d712203748087f496ba594d10f8d4b3
wanghaijan <1469202826@qq.com>
2019-10-19 10:24:48 +0800

    调成

--- src/components/HeadTop.vue
+++ src/components/HeadTop.vue
@@ -11,7 +11,7 @@
   <el-menu-item index="1" > <router-link to="/">Home</router-link></el-menu-item>
    <el-menu-item index="3" > <router-link to="/about">About</router-link></el-menu-item>
   <el-submenu index="2">
-    <template slot="title">whj</template>
+    <template slot="title">请登录</template>
     <el-menu-item index="2-1">c</el-menu-item>
     <el-menu-item index="2-2">选项2</el-menu-item>
     <el-menu-item index="2-3">选项3</el-menu-item>
