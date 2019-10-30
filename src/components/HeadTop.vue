4649cf55eacd4bfb698d472428f702ffe52d052d
wanghaijan <1469202826@qq.com>
2019-10-30 14:21:39 +0800

    cc

--- src/components/HeadTop.vue
+++ src/components/HeadTop.vue
@@ -1,22 +1,6 @@
 <template>
   <div id="nav">
-    <el-menu
-  :default-active="activeIndex2"
-  class="el-menu-demo"
-  mode="horizontal"
-  @select="handleSelect"
-  background-color="#545c64"
-  text-color="#fff"
-  active-text-color="#ffd04b" style="padding:0 160px">
-  <el-menu-item index="1" > <router-link to="/">Home</router-link></el-menu-item>
-   <el-menu-item index="3" > <router-link to="/about">About</router-link></el-menu-item>
-  <el-submenu index="2">
-    <template slot="title">请登录</template>
-    <el-menu-item index="2-1">c</el-menu-item>
-    <el-menu-item index="2-2">选项2</el-menu-item>
-    <el-menu-item index="2-3">选项3</el-menu-item>
-  </el-submenu>
-</el-menu>
+ 
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="home">
-    <el-carousel indicator-position="outside" style="display:none">
-      <el-carousel-item v-for="item in 4" :key="item">
-        <h3>{{ item }}</h3>
-      </el-carousel-item>
-    </el-carousel>
+  
   </div>
 </template>
 
@@ -13,30 +9,7 @@ export default {
   components: {}
 };
 </script>
+
 <style scoped>
-.home {
-  width: 1200px;
-  margin: 0 auto;
-}
-.el-carousel__item h3 {
-  color: #475669;
-  font-size: 18px;
-  opacity: 0.75;
-  line-height: 400px;
-  margin: 0;
-}
-.el-carousel__container{
-  height: 400px;
-}
-.el-carousel__item:nth-child(2n) {
-  background-color: #99a9bf;
-}
-.el-carousel__indicators--outside{
-  position: absolute;
-  left: 0;
-  right: 0;
-}
-.el-carousel__item:nth-child(2n + 1) {
-  background-color: #d3dce6;
-}
+
 </style>
